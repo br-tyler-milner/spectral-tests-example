759266109e3272d977d5673f65c39c61dbf6c67e
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2025-02-17 12:00:41 -0600

    Add comments to `jest.config.ts`.

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,7 @@ export default async (): Promise<Config> => {
     testEnvironment: 'node', // Technically not needed since default test environment is node
     testPathIgnorePatterns: ['__helpers__'], // Don't look for tests in __helpers__ directory
     verbose: true, // Verbose output to help with debugging
-    transform: {
+    transform: { // Use isolated modules to improve test run time
       '^.+\\.tsx?$': [
         'ts-jest',
         {
@@ -17,7 +17,7 @@ export default async (): Promise<Config> => {
         },
       ],
     },
-    moduleNameMapper: {
+    moduleNameMapper: { // Remove .js extension from imports to fix Jest "cannot find module" error (https://stackoverflow.com/a/77852936)
       "(.+)\\.js": "$1",
     },
   };
