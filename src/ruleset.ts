f0a8e6ef66748a99133ae52e7484343e60c2c5c4
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2025-02-28 20:07:56 -0600

    Split ruleset export into `buildRuleset()` function and `export default` statement.

--- src/ruleset.ts
+++ src/ruleset.ts
@@ -4,44 +4,48 @@ import { DiagnosticSeverity } from '@stoplight/types'
 import myCustomFunction from './functions/myCustomFunction.js'
 import { oas as spectralOas } from '@stoplight/spectral-rulesets'
 
-export default {
-  extends: [spectralOas],
-  rules: {
-    // 'info-contact': 'off', // uncomment to disable built-in Spectral 'oas' ruleset 'info-contact' rule
-    'response-must-have-500': {
-      description: 'All responses should have a `500` response defined.',
-      message: 'All endpoints must have a `500` response defined.',
-      given:
-        '$.paths[*].[get,put,post,delete,options,head,patch,trace].responses',
-      then: [
-        {
-          function: defined,
-          field: '500',
-        },
-        {
-          function: pattern,
-          field: '500.description',
+function buildRuleset() {
+  return {
+    extends: [spectralOas],
+    rules: {
+      // 'info-contact': 'off', // uncomment to disable built-in Spectral 'oas' ruleset 'info-contact' rule
+      'response-must-have-500': {
+        description: 'All responses should have a `500` response defined.',
+        message: 'All endpoints must have a `500` response defined.',
+        given:
+          '$.paths[*].[get,put,post,delete,options,head,patch,trace].responses',
+        then: [
+          {
+            function: defined,
+            field: '500',
+          },
+          {
+            function: pattern,
+            field: '500.description',
+            functionOptions: {
+              match: '^Internal Server Error$',
+            },
+          },
+        ],
+        formats: [oas3],
+        severity: DiagnosticSeverity.Error,
+      },
+      'http-status-obsolete': {
+        description: 'Obsolete or unused HTTP status codes should not be used.',
+        message: '{{property}} is an obsolete or unused HTTP status code',
+        given: '$.paths.*.*.responses',
+        then: {
+          function: myCustomFunction,
+          field: '@key',
           functionOptions: {
-            match: '^Internal Server Error$',
+            values: ['306', '418', '510'],
           },
         },
-      ],
-      formats: [oas3],
-      severity: DiagnosticSeverity.Error,
-    },
-    'http-status-obsolete': {
-      description: 'Obsolete or unused HTTP status codes should not be used.',
-      message: '{{property}} is an obsolete or unused HTTP status code',
-      given: '$.paths.*.*.responses',
-      then: {
-        function: myCustomFunction,
-        field: '@key',
-        functionOptions: {
-          values: ['306', '418', '510'],
-        },
+        formats: [oas3],
+        severity: DiagnosticSeverity.Warning,
       },
-      formats: [oas3],
-      severity: DiagnosticSeverity.Warning,
     },
-  },
+  }
 }
+
+export default buildRuleset()
